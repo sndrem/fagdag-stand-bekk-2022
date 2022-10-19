483cfe0be11ffada039c4ba5973d25a249cdb32d
Sindre Moldeklev <sndrem@gmail.com>
2022-10-19 21:08:08 +0200

    Prøver oss på et install-script for enkelt oppsett på fagdagen

--- init.sh
+++ init.sh
@@ -15,3 +15,41 @@ else
     touch .env
     echo "UNSPLASH_ACCESS_KEY=<DITT ACCESS TOKEN>" >> .env
 fi
+
+echo "Sjekker om Brew er installert..."
+if ! [ -x "$(command -v brew)" ]; 
+then
+  echo '💥 Error: brew er ikke installert.' >&2
+  echo "Prøver å installere brew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+else
+    echo "Brew er installert 👍🏼"
+fi
+
+echo "Sjekker om Go er installert..."
+if ! [ -x "$(command -v go)" ]; 
+then
+  echo '💥 Error: go er ikke installert.' >&2
+  echo "Prøver å installere Go"
+  brew install golang
+  mkdir -p $HOME/go/{bin,src,pkg}
+  export GOPATH=$HOME/go
+  export GOROOT="$(brew --prefix golang)/libexec"
+  export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+  source $HOME/.bashrc
+  source $HOME/.zshrc
+else
+    echo "Go er installert 👍🏼"
+fi
+
+echo "Sjekker om Primitive er installert..."
+if ! [ -x "$(command -v primitive)" ]; 
+then
+  echo '💥 Error: primitive er ikke installert.' >&2
+  echo "Prøver å installere Primitive"
+  go install github.com/fogleman/primitive@latest
+  export PATH=$PATH:$(go env GOPATH)/bin
+else
+    echo "Primitive er installert 👍🏼"
+fi
